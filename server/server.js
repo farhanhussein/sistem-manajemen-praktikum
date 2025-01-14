a9159d996ff9848e74d5a16e21303f9ca1763594
Farhan Hussein <farhanhussein@Farhans-MacBook-Air.local>
2025-01-14 23:32:14 +0700

    Update Changes

--- server/server.js
+++ server/server.js
@@ -4,11 +4,28 @@ const cors = require("cors");
 const PORT = 8080;
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/api/home", (req, res) => {
-    res.json({message: "Hello World!", pople: ["Harry", "Jack", "Rahmat"]});
-});
+// API Routes
+app.get('/api/praktikum', (req, res) => {
+    res.json({
+      message: 'Welcome to the Management Information System API',
+      data: ['Lab RPL', 'Lab Mulmed', 'Lab jaringan'],
+    });
+  });
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});
+  app.get("/api/praktikum/lab-rpl", (req, res) => {
+    res.json({
+      message: "Lab RPL Data",
+      data: [
+        "Modul 1: Introduction to RPL",
+        "Modul 2: System Analysis",
+        "Modul 3: Software Design",
+      ],
+    });
+  });
+  
+  // Start the server
+  app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT}`);
+  });
